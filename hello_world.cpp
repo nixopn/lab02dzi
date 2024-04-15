85fbedfadf2fff8022510ad56956143ffb49f3f4
nixopn <eisnersandy2@gmail.com>
2024-04-15 15:04:18 +0300

    Dobavil commentarii

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <string>
+#include <string>//biblioteki
 
 int main() {
+//vvedi imya
     std::string name;
     std::cout << "Enter your name: ";
     std::cin >> name;
-
-    std::cout << "Hello world from " << name << std::endl;
-    return 0;
+std::cout << "Hello world from " << name << std::endl;
+return 0;
 }
